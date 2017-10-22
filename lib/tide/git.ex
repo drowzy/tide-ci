2bffe59d502166f6ccfa2c925e2856e9156737bc
Simon Thörnqvist <simon@plug-n-trade.com>
2017-10-22 11:10:01 +0200

    add clone & archive actions

--- lib/tide/git.ex
+++ lib/tide/git.ex
@@ -19,27 +19,56 @@ defmodule Tide.Git do
   Ensures that the repository exists on the filesystem.
   This is sort of a clone, if the directory is not empty
   """
-  def ensure do
+  @spec ensure(Tide.Git.t) :: {:ok, Tide.Git.t} | {:error, String.t}
+  def ensure(%__MODULE__{default_branch: branch} = git) do
+    case clone_or_new(git) do
+      {:ok, repo} -> Git.pull repo, ["--rebase", "origin", branch]
+      {:error, reason} -> {:error, reason}
+    end
   end
 
   @doc """
-  Creates an archive stream of the repository
+  Creates an archive, and outputs a stream of the repository
   """
-  def archive do
+  def archive(%__MODULE__{} = git, opts \\ []) do
+    with {:ok, repo} <- clone_or_new(git),
+         {:ok, _} <- Git.archive(repo, [
+           "--output=test.tar.gz",
+           "--format=tar.gz",
+           "HEAD"
+         ]) do
+      :ok
+    else
+      {:error, reason} -> {:error, reason}
+    end
   end
 
   @doc """
   Checks if the remote repo is accessible
   """
   @spec accessable?(Tide.Git.t) :: boolean
-  def accessable?(%__MODULE__{path: path, type: :ssh}) do
-    case Git.ls_remote do
+  def accessable?(%__MODULE__{uri: uri, type: :ssh}) do
+    case Git.ls_remote(nil, [uri]) do
       {:ok, _} -> true
       {:error, _reason} -> false
     end
   end
 
-  defp dirname(%__MODULE__{}) do
+  defp clone_or_new(%__MODULE__{path: path, uri: uri}) do
+    path = Path.expand path
+
+    if File.exists? path do
+      {:ok, Git.new path}
+    else
+      Git.clone [uri, path]
+    end
+  end
+
+  @doc """
+  Removes the directory
+  """
+  @spec clean(Tide.Git.t) :: {:ok, any()} | {:error, String.t}
+  def clean(%__MODULE__{} = attrs) do
   end
 
 end

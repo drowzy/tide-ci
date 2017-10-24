796ea5f3efe5ea9e42d11b6bed78bda4e8e69efc
Simon Thörnqvist <simon@plug-n-trade.com>
2017-10-24 22:22:55 +0200

    WIP! stream build events with genstage

---
+++ lib/tide/consumer.ex
@@ -0,0 +1,20 @@
+defmodule Tide.Consumer do
+  require Logger
+  use GenStage
+
+  def start_link do
+    Logger.info("starting consumer")
+    GenStage.start_link(__MODULE__, :ok)
+  end
+
+  def init(:ok), do: {:consumer, nil}
+  def handle_events(events, _from, state) do
+    Logger.info("Received #{length events} from producer: #{inspect events}")
+    {:noreply, [], state}
+  end
+
+  def handle_info({_, :stop}, state) do
+    Logger.info("STOPPING CONSUMER")
+    {:stop, :normal, state}
+  end
+end
--- lib/tide/docker/client.ex
+++ lib/tide/docker/client.ex
@@ -3,13 +3,13 @@ defmodule Tide.Docker.Client do
   Docker client
   """
   @version "v1.29"
-  @uri "http+unix://#{URI.encode_www_form("/var/run/docker.sock")}"
+  @uri "http+unix://#{URI.encode_www_form("/var/run/docker.sock")}/#{@version}"
 
   def default_uri, do: @uri
   def build(uri, tar_stream, opts \\ []) do
     base_url = uri <> "/build"
     headers = %{"content-type" => "application/tar", "accept" => "application/json"}
 
-    HTTPoison.post!(base_url, {:stream, tar_stream}, headers, opts)
+    HTTPoison.post(base_url, {:stream, tar_stream}, headers, opts)
   end
 end
